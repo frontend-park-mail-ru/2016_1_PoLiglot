885f801cdda12411edd52701469583adabca732a
sfilatov96 <stepa-filatov@yandex.ru>
2016-02-24 13:20:16 +0300

    не работает

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,10 +10,23 @@ module.exports = function (grunt) {
 		},
 
 		watch: {
-			xml:{
-				files:['templates/*.xml'],
-				tasks:['fest']
-			}
+			fest: {
+                files: ['templates/*.xml'],
+                tasks: ['fest'],
+                options: {
+                    interrupt: true,
+                    atBegin: true
+                }
+            },
+            server: {
+                files: [
+                    'public_html/js/**/*.js',
+                    'public_html/css/**/*.css'
+                ],
+                options: {
+                    livereload: true
+                }
+            }
 			// запуск watcher'a, который следит за изенениями файлов  templates/*.xml
 			// и если они изменяются, то запускает таск сборки шаблонов (grunt fest)
 		},
