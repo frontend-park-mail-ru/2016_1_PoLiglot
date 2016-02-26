8c5ac80da0c284cfb99481602f9400afbc602387
sfilatov96 <stepa-filatov@yandex.ru>
2016-02-26 23:36:18 +0300

    scoreboard collection

--- public_html/js/collections/scores.js
+++ public_html/js/collections/scores.js
@@ -1,11 +1,29 @@
 define([
-    'backbone'
+	'backbone',
+	'models/score'
 ], function(
-    Backbone
+	Backbone,
+	playerModel
 ){
 
-    var Collection = Backbone.Collection.extend({
+    var ScoreCollection = Backbone.Collection.extend({
+    	model: playerModel,
+    	comparator: function(player) {
+            return -player.get('score');   
+        }
     });
 
-    return new Collection();
+    var players = new ScoreCollection([
+    	{name: 'Stepan123', score: 16},
+    	{name: 'IGORR', score: 1.4},
+    	{name: 'Dmitr55343', score: 33},
+    	{name: 'supergamer123', score: -7},
+    	{name: 'Vitaly', score: 45},
+    	{name: 'Sanja77', score: 2},
+    	{name: 'Max', score: 25},
+    	{name: 'Eddy1986', score: 0},
+    	{name: 'gamefun', score: 0}
+    ]);
+
+    return players;
 });
