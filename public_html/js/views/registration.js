9d1136febce8a59e4cc7cb7cfb7ed2ade53242e3
sfilatov96 <stepa-filatov@yandex.ru>
2016-03-03 11:39:53 +0300

    Добавлена валидация

--- public_html/js/views/registration.js
+++ public_html/js/views/registration.js
@@ -9,19 +9,63 @@ define([
     var registrationView = Backbone.View.extend({
 
         template: tmpl,
+        events: {
+            submit: "send"
+        },
         initialize: function () {
-            $('#page').html(tmpl());   // TODO
+            $('#page').html(tmpl());
+              // TODO
         },
         render: function () {
             this.$el.html(tmpl());
-            return this;// TODO
+            this.$name = this.$("input[name=login]");
+            this.$pass = this.$("input[name=password]");
+            this.$passRepeat = this.$("input[name=password_repeat]");
+            this.$email = this.$("input[name=email]");
+            return this; // TODO
         },
         show: function () {
             $('#page').html(this.render().$el);// TODO
-            this.$('.main').fadeIn("slow");  // TODO
+            this.$('.main').fadeIn("slow");
         },
-        hide: function () {
-            $('#page').html(tmpl()); // TODO
+        send: function(event){
+            event.preventDefault();
+            this.$('.error').fadeOut('fast');
+
+            if(!this.$name.val() || !this.$pass.val() || !this.$passRepeat.val() || !this.$email){
+                this.$('.error').fadeIn('fast');
+                this.$('.error').html('<strong>Ой! </strong>Выделенные поля не заполнены!');
+                return;
+            }
+            var email = this.$email.val();
+            var name = this.$name.val();
+            var pass = this.$pass.val();
+            var passRepeat = this.$passRepeat.val();
+
+            if(pass.length < 8 ){
+                this.$('.error').fadeIn('fast');
+                this.$('.error').html('<strong>Ой! </strong> Пароль состоит менее чем из 8 символов!');
+                this.$pass.parent('.form-group').addClass('has-error');
+                return;
+            }
+            if(!/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/.test(email)){
+                this.$('.error').fadeIn('fast');
+                this.$('.error').html('<strong>Ой! </strong> E-mail введён некорректно!');
+                return;
+            }
+            if(!/\w/.test(pass) || !/\w/.test(name)){
+                this.$('.error').fadeIn('fast');
+                this.$('.error').html('<strong>Ой! </strong> Пароль и Логин может содержать символы: A-Z a-z 0-9!');
+                return;
+            }
+
+            if(pass = passRepeat){
+                this.$('.error').fadeIn('fast');
+                this.$('.error').html('<strong>Ой! </strong> Пароли не совпадают');
+                return;
+            }
+
+
         }
 
     });
