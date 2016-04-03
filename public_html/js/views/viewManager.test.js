8053961428031bdc5c1b19a447dd5fca62912947
sfilatov96 <stepa-filatov@yandex.ru>
2016-04-03 17:46:18 +0300

    исправил помарку

--- public_html/js/views/viewManager.test.js
+++ public_html/js/views/viewManager.test.js
@@ -3,7 +3,7 @@ define(function (require) {
     QUnit.test("Проверка работы ViewManager", function (assert) {
         var viewManager = require('views/viewManager');
         viewManager.hide();
-        _.each(viewManager.views || , function(view) {
+        _.each(viewManager.views , function(view) {
                 view.hide();
                 assert.equal($(this).is(":visible"), false);
             });
